0b8a46df7f54a03043783432e12b0522db2e3bdc
YoungsonMan <choi13570@naver.com>
2024-10-19 01:43:40 +0900

    [Feat] 총 소리 구현
    총소리 왼쪽에서 쏘면 왼쪽에서 들리게 / 오른쪽...
    서라운드? 입체적 소리 쏘는방향에서 들리도록 구현해야함..

--- Assets/2_Scripts/PistolController.cs
+++ Assets/2_Scripts/PistolController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PistolController : MonoBehaviour
@@ -11,6 +12,9 @@ public class PistolController : MonoBehaviour
 
     [SerializeField] ParticleSystem vfxFire;
     [SerializeField] ParticleSystem vfxShellOut;
+
+    [SerializeField] AudioClip fireSound;
+
     void Start()
     {
 
@@ -21,6 +25,7 @@ public class PistolController : MonoBehaviour
         if( Input.GetKeyDown(KeyCode.Space))
         {
             Fire();
+
         }
         
     }
@@ -33,5 +38,7 @@ public class PistolController : MonoBehaviour
         // rigid.velocity = bullet.transform.forward * bulletSpeed * Time.deltaTime;
         vfxFire.Play();
         vfxShellOut.Play();
+        SoundManager.Instance.PlaySFXPistolFire(fireSound);
     }
+ 
 }
---
+++ Assets/2_Scripts/SoundManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance { get; private set; }
+
+    [SerializeField] AudioSource bgm;
+    [SerializeField] AudioSource sfxPistolFire;
+    [SerializeField] AudioSource sfxPistolShellDrop;
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }   
+    }
+
+    public void SetBgm(float volume, float pitch)
+    {
+        bgm.volume = volume;
+        bgm.pitch = pitch;
+    }
+    public void playBGM(AudioClip clip)
+    { 
+        bgm.clip = clip;
+        bgm.Play();
+    }
+    public void StopBGM(AudioClip clip)
+    {
+        if (bgm.isPlaying == false)
+            return;
+        bgm.Stop();
+    }
+    public void PauseBGM()
+    {
+        if (bgm.isPlaying == false)
+            return;
+        bgm.Pause();
+    }
+    public void PlaySFXPistolFire(AudioClip clip)
+    {
+        sfxPistolFire.PlayOneShot(clip);
+    }
+
+}
